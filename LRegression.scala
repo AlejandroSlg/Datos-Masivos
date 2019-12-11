ed5aed2fd61f5b46ccc1cd648ba28a8d7efeff42
AlejandroSlg <raul.salcido@tectijuana.edu.mx>
2019-12-11 00:41:07 -0800

    Update LRegression.scala

--- LRegression.scala
+++ LRegression.scala
@@ -1,54 +1,45 @@
-/*Importamos las librerias necesarias con las que vamos a trabajar*/
-import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.mllib.util.MLUtils
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types.DateType
-import org.apache.spark.sql.{SparkSession, SQLContext}
-import org.apache.spark.ml.feature.VectorIndexer
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.Transformer
-import org.apache.spark.ml.classification.LinearSVC
+/*Importamos las librerías*/
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.log4j._
-
-/*Creamos una sesion de spark y cargamos los datos del CSV en un datraframe*/
+import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer, VectorIndexer, OneHotEncoder}
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.SparkSession
 val spark = SparkSession.builder().getOrCreate()
-val df = spark.read.option("header","true").option("inferSchema","true").option("delimiter",";").format("csv").load("bank-full.csv")
 
-/*Desblegamos los tipos de datos.*/
-df.printSchema()
-df.show(1)
+/*Se importan los datos del CSV*/
+val data  = spark.read.option("header","true").option("inferSchema", "true").option("delimiter",";").format("csv").load("bank-full.csv")
 
-/*Cambiamos la columna y por una con datos binarios.*/
-val change1 = df.withColumn("y",when(col("y").equalTo("yes"),1).otherwise(col("y")))
-val change2 = change1.withColumn("y",when(col("y").equalTo("no"),2).otherwise(col("y")))
-val newcolumn = change2.withColumn("y",'y.cast("Int"))
+/*Se categoriza las variables de string a valor numérico*/
+val yes = data.withColumn("y",when(col("y").equalTo("yes"),1).otherwise(col("y")))
+val clean = yes.withColumn("y",when(col("y").equalTo("no"),2).otherwise(col("y")))
+val cleanData = clean.withColumn("y",'y.cast("Int"))
 
-/*Desplegamos la nueva columna*/
-newcolumn.show(1)
+/*Creación del Array con los datos seleccionados*/
+val featureCols = Array("age","previous","balance","duration")
 
-/*Generamos la tabla features*/
-val assembler = new VectorAssembler().setInputCols(Array("balance","day","duration","pdays","previous")).setOutputCol("features")
-val fea = assembler.transform(newcolumn)
+/*Creación del Vector en base a los features*/
+val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
 
-/*Mostramos la nueva columna*/
-fea.show(1)
+/*Transformación a un nuevo DF*/
+val df2 = assembler.transform(cleanData)
 
-/*Cambiamos la columna y a la columna label*/
-val cambio = fea.withColumnRenamed("y", "label")
-val feat = cambio.select("label","features")
-feat.show(1)
+/*Rename de columnas*/
+val featuresLabel = df2.withColumnRenamed("y", "label")
 
-/*Logistic Regresion*/
-val logistic = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+/*Selección de index*/
+val dataI = featuresLabel.select("label","features")
 
-/* Fit del modelo*/
-val logisticModel = logistic.fit(feat)
+/*Creación del Array con los datos de entrenamiento y test*/
+val Array(training, test) = dataI.randomSplit(Array(0.7, 0.3), seed = 12345)
 
-/*Impresion de los coegicientes y de la intercepcion*/
-println(s"Coefficients: ${logisticModel.coefficients} Intercept: ${logisticModel.intercept}")
-val logisticMult = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8).setFamily("multinomial")
-val logisticMultModel = logisticMult.fit(feat)
-println(s"Multinomial coefficients: ${logisticMultModel.coefficientMatrix}")
-println(s"Multinomial intercepts: ${logisticMultModel.interceptVector}")
+/*Modelo de Regresion*/
+val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+
+val lrModel = lr.fit(training)
+
+/*Impresión de los coeficientes e intercepciones*/
+println(s"Coefficients: \n${lrModel.coefficientMatrix}")
+println(s"Intercepts: \n${lrModel.interceptVector}")
+
+
+/*Impresión de la precisión*/
+println(s"Accuracy: $accuracy")
